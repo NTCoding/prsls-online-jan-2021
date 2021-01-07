f9e51dfdcc9e930192aef4fe3d7e228e31458f54
Nick Tune <nick.tune@softwarepark.cc>
2021-01-07 13:24:45 +0000

    exercise 6

--- production-ready-serverless-workshop/functions/get-index.js
+++ production-ready-serverless-workshop/functions/get-index.js
@@ -5,6 +5,10 @@ const aws4 = require('aws4')
 const URL = require('url')
 
 const restaurantsApiRoot = process.env.restaurants_api
+const cognitoUserPoolId = process.env.cognito_user_pool_id
+const cognitoClientId = process.env.cognito_client_id
+const awsRegion = process.env.AWS_REGION
+
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 const template = fs.readFileSync('static/index.html', 'utf-8')
@@ -29,7 +33,15 @@ module.exports.handler = async (event, context) => {
   const restaurants = await getRestaurants()
   console.log(`found ${restaurants.length} restaurants`)  
   const dayOfWeek = days[new Date().getDay()]
-  const html = Mustache.render(template, { dayOfWeek, restaurants })
+  const view = {
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    dayOfWeek,
+    restaurants,
+    searchUrl: `${restaurantsApiRoot}/search`
+  }
+  const html = Mustache.render(template, view)
   const response = {
     statusCode: 200,
     headers: {
